9ce8b78af05f03079623747a6f8c9bca329d49de
Shree <dineshshk123@gmail.com>
2018-07-03 15:51:52 +0530

    dashboard with multiple design sign in and sign up...

--- app/src/main/java/com/example/shree/dashboard/Activity_Timeline.java
+++ app/src/main/java/com/example/shree/dashboard/Activity_Timeline.java
@@ -1,13 +1,31 @@
 package com.example.shree.dashboard;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class Activity_Timeline extends AppCompatActivity {
 
+    RelativeLayout relativeLayout,relativeLayout2;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+        relativeLayout.setVisibility(View.VISIBLE);
+        relativeLayout2.setVisibility(View.VISIBLE);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__timeline);
+        relativeLayout = findViewById(R.id.really1);
+        relativeLayout2 = findViewById(R.id.relativeLayout9);
+
+        handler.postDelayed(runnable,2000);
+
+
     }
 }
